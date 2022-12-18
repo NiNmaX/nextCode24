3981470fd3437acaf48073291d1dde021e853c02
Abdusamadova Nigina <nigina2007abdusamadova@gmail.com>
2022-12-18 10:44:58 +0500

    nextCode24

---
+++ main.js
@@ -0,0 +1,97 @@
+
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+
+
+  const linksHeight = links.getBoundingClientRect().height;
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
+
+
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+
+
+  if (scrollHeight > 500) {
+    console.log("hello");
+
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
+
+
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+
+    e.preventDefault();
+
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+
+    linksContainer.style.height = 0;
+  });
+});
+
+  var slideShow = function(container) {
+    this.images = [];
+    this.curImage = 0;
+    for (i = 0; i < container.childElementCount; i++){
+      this.images.push(container.children[i]);
+      this.images[i].style.display = "none";
+    }
+
+  // handle going to to the next slide
+  var nextSlide = function() {
+    for (var i = 0; i < this.images.length; i++) {
+      this.images[i].style.display = "none";
+    }
+    this.images[this.curImage].style.display = "block";
+    this.curImage++;
+    if (this.curImage >= this.images.length) {
+      this.curImage = 0;
+    }
+    window.setTimeout(nextSlide.bind(this), 2000);
+  };
+
+  nextSlide.call(this);
+};
+slideShow(document.getElementById("slideshow"));
